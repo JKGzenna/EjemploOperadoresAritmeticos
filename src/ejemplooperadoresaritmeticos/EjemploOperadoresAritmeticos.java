f134d3cbf6db46450764d90bac7157b008d3fc49
JKGzenna <juanlu360@gmail.com>
2017-06-11 23:56:10 +0200

    EjemploOperadoresAritmeticos

---
+++ src/ejemplooperadoresaritmeticos/EjemploOperadoresAritmeticos.java
@@ -0,0 +1,22 @@
+package ejemplooperadoresaritmeticos;
+
+/**
+ *
+ * @author JUAN
+ */
+public class EjemploOperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 2;
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b); // como la division no es entera nos dara el resultado sin resto
+        System.out.println(a%b); // con este operador '%' obtendremos el resto de nuestra division cuando no es entera
+        
+    }
+    
+}
